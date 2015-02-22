6cb444cb5af839b84520db73cfa4685c1e739227
Chengzhi Gu <cgu@pinterest.com>
2015-02-22 01:54:43 -0800

    added new views

--- server/webapp/urls.py
+++ server/webapp/urls.py
@@ -4,4 +4,7 @@ from webapp import views
 
 urlpatterns = patterns('',
     url(r'^(home/)?$', views.home, name="home"),
+    url(r'^tracker/$', views.tracker, name="nutritionTracker"),
+    url(r'^about/(?P<item>[A-Za-z]+)?$', views.about, name="about"),
+    url(r'^contact/$', views.contact, name="contact")
 )
--- server/webapp/views.py
+++ server/webapp/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 def home(request, *args, **kwargs):
     return render(request, 'webapp/index.html')
 
+def tracker(request):
+    return render(request, 'webapp/nutrition-tracker.html')
+
+def about(request, item):
+    return render(request, 'webapp/about.html')
+
+def contact(request):
+    return render(request, 'webapp/contact.html')
